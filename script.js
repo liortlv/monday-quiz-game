a4414938886897ed76162df363fd1adfb6389724
Lior <liortal.tal@gmail.com>
2022-04-02 14:08:33 +0300

    Add welcome page basic html and css  design

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+async function getData() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data.results[4]);
+}
+const api_url = 'https://opentdb.com/api.php?amount=100';
+getData();
+*/
+//selecting all required elements
