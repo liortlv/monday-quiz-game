5f676a8f23bf1840b1cabc9c8bca1fe1a8c42361
Lior <liortal.tal@gmail.com>
2022-04-03 16:57:32 +0300

    Fix boolean questions bug

--- quiz.js
+++ quiz.js
@@ -3,10 +3,11 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+const choiceContainer3 = document.querySelector('.choice-container3');
+const choiceContainer4 = document.querySelector('.choice-container4');
 
-const API_URL = 'https://opentdb.com/api.php?amount=100&encode=base64';
 const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 5;
+const MAX_QUESTIONS = 25;
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -15,26 +16,22 @@ let questionCounter = 0;
 let availableQuestions = [];
 let questions;
 
-async function getApiQuestions() {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    return data.results;
-}
-
 function startGame() {
-    questionCounter = 1;
+    questionCounter = 0;
     score = 0;
     availableQuestions = questions;
     getNewQuestion();
 }
 
+/* Gets new question, either true/false or 4 possible answers. 
+   Presents the question and possible choices. */
 function getNewQuestion() {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
         return window.location.assign('/end.html');
     }
-    
+
     questionCounter++;
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
@@ -43,34 +40,45 @@ function getNewQuestion() {
     question.innerText = atob(currentQuestion.question);
 
     const allChoices = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer];
-    /* Randomize array in-place using Durstenfeld shuffle algorithm */
+
     shuffleArray(allChoices);
-    let choiceCounter = 0;
-    choices.forEach(choice => {
-        choice.innerText = atob(allChoices[choiceCounter]);
-        choiceCounter++;
-    })
+
+    // For true/false question hides the 3rd and 4th choices
+    if (currentQuestion.type == btoa('boolean')) {
+        choiceContainer3.classList.add('hidden');
+        choiceContainer4.classList.add('hidden');
+        choices[0].innerText = 'True';
+        choices[1].innerText = 'False';
+    } else {
+        choiceContainer3.classList.remove('hidden');
+        choiceContainer4.classList.remove('hidden');
+        let choiceCounter = 0;
+        choices.forEach(choice => {
+            choice.innerText = atob(allChoices[choiceCounter]);
+            choiceCounter++;
+        })
+    }
+
     acceptingAnswers = true;
     console.log(atob(currentQuestion.question));
     console.log(atob(currentQuestion.correct_answer));
 }
 
+// When the user clicks an answer
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
         let classToApply = btoa(selectedChoice.innerText) == currentQuestion.correct_answer ? 'correct' : 'incorrect';
-
 
         if (classToApply === 'correct') {
             incrementScore(SCORE_POINTS);
         }
 
+        // Color the container according to correct/incorrect user answer
         selectedChoice.parentElement.classList.add(classToApply);
-
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
@@ -83,6 +91,7 @@ function incrementScore(num) {
     scoreText.innerText = score;
 }
 
+// Randomize array in-place using Durstenfeld shuffle algorithm */
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -92,6 +101,15 @@ function shuffleArray(array) {
     }
 }
 
+// Fetch questions from API in base64 encoding
+async function getApiQuestions() {
+    const API_URL = 'https://opentdb.com/api.php?amount=100&encode=base64';
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.results;
+}
+
+// main gets API questions and starts the game
 (async function main() {
     questions = await getApiQuestions();
     startGame()
