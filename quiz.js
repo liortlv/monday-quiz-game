9a54395c175b7a09fe09e1e54343ecdb4d884626
Lior <liortal.tal@gmail.com>
2022-04-03 17:21:16 +0300

    Delete unused if statement and variable

--- quiz.js
+++ quiz.js
@@ -13,20 +13,18 @@ let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
 let questions;
 
 function startGame() {
     questionCounter = 0;
     score = 0;
-    availableQuestions = questions;
     getNewQuestion();
 }
 
 /* Gets new question, either true/false or 4 possible answers. 
    Presents the question and possible choices. */
 function getNewQuestion() {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    if (questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
         return window.location.assign('/end.html');
